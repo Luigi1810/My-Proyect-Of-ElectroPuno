766f5b2574cba3056c17e82c04390ea26b85994f
Luigi1810 <139819009+Luigi1810@users.noreply.github.com>
2025-07-21 16:20:46 -0500

    Update ConsumoEnTiempoReal.tsx
    el cambio

--- src/app/Settings/ConsumoEnTiempoReal.tsx
+++ src/app/Settings/ConsumoEnTiempoReal.tsx
@@ -7,7 +7,7 @@ import { LineChart } from 'react-native-chart-kit';
 import { useRouter } from 'expo-router';
 import { useLanguage } from 'C:/Users/amigo/Downloads/PROYECTO PYTHON/PROYECTOS CON REACT NATIVE/MyApp/src/utils/LanguageContext';
 
-interface EnergyData {
+interface EnergyData { //luiouoiuouoi
   voltage: number;
   current: number;
   power: number;
